55bf628e8a7e7c840633fdb06747c25474392bae
jopedroliveira <j.pedrodiasoliveira@gmail.com>
2014-04-26 19:49:29 +0100

    erro no main

--- gen/com/example/nomeubairro_stable/R.java
+++ gen/com/example/nomeubairro_stable/R.java
@@ -19,7 +19,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int buttonBarStyle=0x7f010000;
     }
     public static final class color {
+        public static final int black=0x7f040005;
         public static final int black_overlay=0x7f040000;
+        public static final int brown_bairro=0x7f040002;
+        public static final int light_red=0x7f040004;
+        public static final int red_bairro=0x7f040001;
+        public static final int red_bottom=0x7f040007;
+        public static final int red_top=0x7f040006;
+        public static final int white=0x7f040003;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -32,8 +39,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int cenas=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
+        public static final int redbutton=0x7f020001;
     }
     public static final class id {
         public static final int AddImageButton=0x7f090003;
@@ -43,8 +50,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ImageView=0x7f090004;
         public static final int Reportar_Novo=0x7f090007;
         public static final int action_settings=0x7f090008;
-        public static final int fullscreen_content_controls=0x7f090006;
         public static final int textView1=0x7f090005;
+        public static final int textView2=0x7f090006;
     }
     public static final class layout {
         public static final int activity_add_report_=0x7f030000;
@@ -56,9 +63,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class string {
         public static final int action_settings=0x7f060004;
         public static final int app_name=0x7f060000;
+        public static final int description=0x7f060008;
         public static final int dummy_button=0x7f060001;
         public static final int dummy_content=0x7f060002;
         public static final int hello_world=0x7f060005;
+        public static final int reportarnovo=0x7f060006;
+        public static final int title=0x7f060007;
         public static final int title_activity_add_report_=0x7f060003;
     }
     public static final class style {
@@ -88,9 +98,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int Botao=0x7f070005;
         public static final int ButtonBar=0x7f070003;
         public static final int ButtonBarButton=0x7f070004;
-        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenActionBarStyle=0x7f070006;
         public static final int FullscreenTheme=0x7f070002;
     }
     public static final class styleable {
